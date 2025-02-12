cbeb8030429bfffeb6a2bc17091d9b236dbe0ca9
Venkatavamsi850 <venkatavamsi448@gmail.com>
2025-02-12 15:37:45 +0530

    Added changes to dist files and RestaurantMenu.js

--- src/components/Restaurantcategory.js
+++ src/components/Restaurantcategory.js
@@ -37,25 +37,4 @@ const RestaurantCategory = ({ data }) => {
 
 export default RestaurantCategory;
 
-/*import ItemList from "./ItemList";
-const RestaurantCategory = ({ data }) => {
-    return (
-      <div className="flex items-center justify-between p-4 bg-gray-100 rounded-md shadow hover:bg-gray-200 cursor-pointer">
-        <span className="text-lg font-semibold text-gray-800">{data.title}({data.itemCards.length})</span>
-        <span className="text-xl text-gray-500">⬇️</span>
-        <ItemList items={data.itemCards} />
-      </div>
-      
-    );
-  };
-  
-  export default RestaurantCategory;*/
-  
 
-/*const RestaurantCategory=({data})=>{
-    return <div>
-        <span>{data.title}</span>
-        <span>⬇️</span>
-    </div>
-}
-export default RestaurantCategory*/
